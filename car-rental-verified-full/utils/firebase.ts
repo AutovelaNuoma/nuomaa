ec970e46ee567df6cba88b6afb995daa9905a7fc
AutovelaNuoma <robert.baniukevic@gmail.com>
2025-06-20 18:36:42 +0300

    Update firebase.ts

--- car-rental-verified-full/utils/firebase.ts
+++ car-rental-verified-full/utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "PAKEISK_API_KEY",
-  authDomain: "PAKEISK_AUTH_DOMAIN",
-  databaseURL: "PAKEISK_DATABASE_URL",
-  projectId: "PAKEISK_PROJECT_ID",
-  storageBucket: "PAKEISK_STORAGE_BUCKET",
-  messagingSenderId: "PAKEISK_SENDER_ID",
-  appId: "PAKEISK_APP_ID"
+  apiKey: "AIzaSyBD8p00I8DBAS_yJ9vXCs1BkxWh3vSrxTU",
+  authDomain: "automobiliu-nuoma.firebaseapp.com",
+  projectId: "automobiliu-nuoma",
+  storageBucket: "automobiliu-nuoma.firebasestorage.app",
+  messagingSenderId: "465502032588",
+  appId: "1:465502032588:web:3d549237af6c4903362869",
+  measurementId: "G-V7E37P3HZ1"
 };
 
 const app = initializeApp(firebaseConfig);
